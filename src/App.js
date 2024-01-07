8b521065f7b4d3cb4051f9a9825dde35419c103d
vicozic <thaesu.code@>
2024-01-07 09:45:01 +0630

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React ,{ useState}from 'react'
 import List from './Component/List/List'
 import Form from './Component/Form';
+import { v4 as uuid } from 'uuid';
 
 const App = () => {
   // const data = ['Home','About','Contact','Menu','Price'];
@@ -11,29 +12,51 @@ const App = () => {
   ]);
   const handleClick = (text) => {
     // console.log(refInput);
-    console.log(text);
+    // console.log(text);
     const inputs = {};
     inputs.name = text;
     // inputs.name = setInput.current.value;
     inputs.isDone = false; 
-    inputs.id = Math.random(Date.now()) * 100000
+    // inputs.id = Math.random(Date.now()) * 100000
+    inputs.id = uuid;
     setData([...data,inputs]);
     // setInput.current.value="";
     text ="";
 
   }
 
-  const handleCheck = () => {
-    console.log("This is handle Check");
+  const handleCheck = (id) => {
+    setData(data.map(i => {
+      if(i.id === id){
+        return {name:i.name,isDone:!i.isDone,id:i.id};
+      }
+      else return i;
+    }));
   }
-         
+
+  const handleDelete = (id) => {
+    // console.log(id);
+    setData(data.filter(i => i.id !== id))
+  };
+       const handleEdit = (id) => {
+        // console.log(id);
+        const exitvalue = data.find(i => i.id === id);
+        const value = prompt("Enter a value",exitvalue.name);
+        if(value !==""){
+          setData(data.map(i => i.id ===id ? {name:value, sDone:!i.isDone,id:i.id}:i ))
+        }
+       }
   return (
     <div>
     <Form formClick ={handleClick}/>
 
 
 
-{data.map((i) => <List name={i.name} isDone={i.isDone } key={i.id}  onCheck = {handleCheck}/>)}
+{data.map((i) => <List name={i.name} isDone={i.isDone } key={i.id} 
+ onCheck = {handleCheck}
+    onDelete = {handleDelete}
+    onEdit = {handleEdit}
+ id={i.id}/>)}
       {/* <List  name={'Home'}  isDone = {true}/>
       <List name={'About'} isDone = {false}/>
       <List name={'Contact'}  isDone = {false}/>
--- src/Component/List/List.jsx
+++ src/Component/List/List.jsx
@@ -1,27 +1,47 @@
 import React from "react";
 
 import "./List.css";
+import {BsTrash} from "react-icons/bs"
 
-
-const List = ({ name, isDone , onCheck}) => { 
+const List = ({ name, isDone , onCheck, id, onDelete,onEdit}) => { 
   // console.log(isDone);
  const handleCheck =() => {
-  onCheck();
+  onCheck(id);
  }
+const handleDelete =() => {
+onDelete(id);
+
+};const handleEdit =() => {
+  onEdit(id);
+};
 
   
   return (
 
     
-    <div>
+    
  
 
     
-      <div className={`listStyle ${isDone && "test"}`}>
-      <input type="checkbox" name="" checked={isDone} onChange={handleCheck}/>
-        {name}
-      </div>
+
+    <div className={`listStyle ${isDone && "test"}`}>
+    <div className="input">
+    <input type="checkbox" name="" checked={isDone} onChange={handleCheck}/>
+      <p> {name}</p>
     </div>
+      <div className="btnContainer">
+        <button onClick={handleEdit}>
+        
+        </button>
+        <button onClick={handleDelete}>
+          <BsTrash/>
+        </button>
+      </div>
+      </div>
+    
+
+  
+  
   );
 };
 
